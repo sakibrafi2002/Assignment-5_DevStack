d574378fa0632dd2225b15d3af91e4b2d8402ec3
sakibrafi2002 <sakibrafi2001@gmail.com>
2026-09-12 12:52:01 +0600

    first commit

---
+++ src/components/shared/Navbar.tsx
@@ -0,0 +1,51 @@
+import logo from "../../assets/logo-text.png";
+const Navbar = () => {
+    const path = "/"
+    const items = [
+        {
+            label : "Home",
+            url : "/"
+        },
+        {
+            label : "Technologies",
+            url : "/tech"
+        },
+        {
+            label : "Projects",
+            url : "/project"
+        },
+        {
+            label : "About",
+            url : "/about"
+        },
+        {
+            label : "Contact",
+            url : "/contact"
+        }
+    ]
+  return (
+    <div className="flex justify-between items-center border border-b border-gray-200 py-3">
+        {/* logo */}
+        <div>
+            <img src={logo} className="" alt="logo"></img>
+        </div>
+        {/* menu */}
+        <ul className="flex items-center gap-4">
+            {
+                items.map(ele => {
+                    return <li key={ele.label} className={path === ele.url ? "text-sm text-[#D91B7E] font-semibold" : "text-sm text-gray-600 font-semibold"}>
+                        {ele.label}
+                    </li>
+                })
+            }
+        </ul>
+        {/* button */}
+        <div className="flex gap-2 items-center">
+            <button className="text-xs font-medium">Sign in</button>
+            <button className="bg-[#D91B7E] px-3 py-1 rounded-full text-xs text-white">Sign up</button>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
