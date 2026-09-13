0542c6d4b042c1c6c63a93721f7642e014aba31e
sakibrafi2002 <sakibrafi2001@gmail.com>
2026-09-13 18:12:51 +0600

    fix: fix import error

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import Navbar from "./components/shared/Navbar";
 import type { ITechnology } from "./utils/types";
 import Technologies from "./components/ui/technologies/Technologies";
 import Footer from "./components/shared/Footer";
-import Hero from "./components/ui/hero/hero";
+import Hero from "./components/ui/hero/Hero";
 
 function App() {
   const [data, setData] = useState<ITechnology[]>([]);
